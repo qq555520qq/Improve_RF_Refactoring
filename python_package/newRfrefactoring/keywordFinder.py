7df8b5fc1cd1d6746f85cc73b9c7a5207e4f0e22
Gene <qq555520qq@gmail.com>
2021-03-30 22:24:20 +0800

    Fixing KeywordFinder.

--- python_package/newRfrefactoring/keywordFinder.py
+++ python_package/newRfrefactoring/keywordFinder.py
@@ -11,65 +11,74 @@ class KeywordFinder(ast.NodeVisitor):
         self.model = None
         self.keyword = None
 
-    def visit_SuiteSetup(self, node):
-        """
-            Visit all keyword's call in suite_setup to do something.
-        """
-        self.append_keywordCall_into_list(node, node.name)
+    # def visit_SuiteSetup(self, node):
+    #     """
+    #         Visit all keyword's call in suite_setup to do something.
+    #     """
+    #     self.append_keywordCall_into_list(node, node.name)
 
-    def visit_SuiteTeardown(self, node):
-        """
-            Visit all keyword's call in suite_teardown to do something.
-        """
-        self.append_keywordCall_into_list(node, node.name)
+    # def visit_SuiteTeardown(self, node):
+    #     """
+    #         Visit all keyword's call in suite_teardown to do something.
+    #     """
+    #     self.append_keywordCall_into_list(node, node.name)
 
-    def visit_TestSetup(self, node):
-        """
-            Visit all keyword's call in test_setup to do something.
-        """
-        self.append_keywordCall_into_list(node, node.name)
+    # def visit_TestSetup(self, node):
+    #     """
+    #         Visit all keyword's call in test_setup to do something.
+    #     """
+    #     self.append_keywordCall_into_list(node, node.name)
 
-    def visit_TestTeardown(self, node):
-        """
-            Visit all keyword's call in test_teardown to do something.
-        """
-        self.append_keywordCall_into_list(node, node.name)
+    # def visit_TestTeardown(self, node):
+    #     """
+    #         Visit all keyword's call in test_teardown to do something.
+    #     """
+    #     self.append_keywordCall_into_list(node, node.name)
 
-    def visit_Setup(self, node):
-        """
-            Visit all keyword's call in setup to do something.
-        """
-        self.append_keywordCall_into_list(node, node.name)
+    # def visit_Setup(self, node):
+    #     """
+    #         Visit all keyword's call in setup to do something.
+    #     """
+    #     self.append_keywordCall_into_list(node, node.name)
 
-    def visit_Teardown(self, node):
-        """
-            Visit all keyword's call in teardown to do something.
-        """
-        self.append_keywordCall_into_list(node, node.name)
+    # def visit_Teardown(self, node):
+    #     """
+    #         Visit all keyword's call in teardown to do something.
+    #     """
+    #     self.append_keywordCall_into_list(node, node.name)
 
-    def visit_TestTemplate(self, node):
-        """
-            Visit all keyword's call in test_template to do something.
-        """
-        self.append_keywordCall_into_list(node, node.name)
+    # def visit_TestTemplate(self, node):
+    #     """
+    #         Visit all keyword's call in test_template to do something.
+    #     """
+    #     self.append_keywordCall_into_list(node, node.name)
 
-    def visit_Template(self, node):
-        """
-            Visit all keyword's call in template to do something.
-        """
-        self.append_keywordCall_into_list(node, node.value)
+    # def visit_Template(self, node):
+    #     """
+    #         Visit all keyword's call in template to do something.
+    #     """
+    #     self.append_keywordCall_into_list(node, node.value)
 
-    def visit_KeywordCall(self, node):
-        """
-            Visit all keyword's call to do something.
-        """
-        self.append_keywordCall_into_list(node, node.keyword)
+    # def visit_KeywordCall(self, node):
+    #     """
+    #         Visit all keyword's call to do something.
+    #     """
+    #     self.append_keywordCall_into_list(node, node.keyword)
 
-    def visit_KeywordName(self, node):
+    def visit_Keyword(self, node):
+        """ 
+            Visit every keyword to get keywordDef and keywordCalled
         """
-            Visit all keyword's define to do something.
-        """
-        self.append_keywordDef_into_list(node, node.name)
+        keywordDef = node.header.get_value(Token.KEYWORD_NAME)
+        self.append_keywordDef_into_list(node, keywordDef)
+
+        for token in node.body:
+            if(token.__class__.__name__ == 'keywordCall'):
+                self.append_keywordCall_into_list(node, token.get_value(Token.KEYWORD))
+            elif(token.__class__.__name__ == 'Teardown'):
+                self.append_keywordCall_into_list(node, token.get_value(Token.NAME))
+                for keyword in token.get_values(Token.ARGUMENT):
+                    self.append_keywordCall_into_list(node, keyword)
 
     def visit_model_for_finding_keyword(self, model, keyword):
         """ 
